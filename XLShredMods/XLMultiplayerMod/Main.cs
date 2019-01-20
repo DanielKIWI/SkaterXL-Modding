e548e727a9b201d436cc326acfed0c063ddfb98d
Daniel Ivert <ivert.daniel@gmail.com>
2019-01-20 23:54:30 +0100

    Added MultiplayerMod Project

---
+++ XLShredMods/XLMultiplayerMod/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Harmony12;
+using System.Reflection;
+using UnityModManagerNet;
+using XLShredLib;
+
+namespace XLMultiplayerMod {
+    static class Main {
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry) {
+            modEntry.OnToggle = OnToggle;
+
+            new GameObject("PlayerCloner").AddComponent<SkaterCloner>();
+            
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
+            enabled = value;
+            if (enabled) {
+
+            }
+            return true;
+        }
+    }
+}
---
+++ XLShredMods/XLMultiplayerMod/SkaterCloner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace XLMultiplayerMod {
+    public class SkaterCloner: MonoBehaviour {
+
+        public void Update() {
+            if (Input.GetKeyDown(KeyCode.C)) {
+                CopyPlayer();
+            }
+        }
+        public void CopyPlayer() {
+            try {
+                GameObject skaterRoot = PlayerController.Instance.transform.root.gameObject;// skaterController.skaterTransform.gameObject;
+                skaterRoot.SetActive(false);
+                GameObject skaterClone = GameObject.Instantiate(skaterRoot);
+                skaterClone.transform.SetParent(null);
+                skaterRoot.SetActive(true);
+                //skaterClone.SetActive(false);
+                Debug.Log("skaterClone: " + skaterClone);
+
+                foreach (var c in skaterClone.GetComponentsInChildren<GraphicRaycaster>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<RawImage>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<Image>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<Text>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<CanvasScaler>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<StandaloneInputModule>()) {
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                foreach (var c in skaterClone.GetComponentsInChildren<Component>()) {
+                    if (c is Renderer) continue;
+                    if (c is Transform) continue;
+                    if (c is MeshFilter) continue;
+                    Debug.Log("Destroyed Component " + c + " from skaterClone");
+                    Destroy(c);
+                }
+                skaterClone.SetActive(true);
+            } catch (Exception e) {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
