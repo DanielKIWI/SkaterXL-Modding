59edc42f39e0ca24351e51416d986e8616c50baa
Daniel Ivert <ivert.daniel@gmail.com>
2019-01-20 23:50:25 +0100

    Fixed DebugGUI wrong Box size

--- XLShredMods/DebugGUI/Patches/LoggerPatch.cs
+++ XLShredMods/DebugGUI/Patches/LoggerPatch.cs
@@ -1,10 +1,6 @@
 ﻿using UnityEngine;
 using Harmony12;
 using System;
-using System.Reflection;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
 
 namespace DebugGUI.Patches {
     [HarmonyPatch(typeof(Logger))]
