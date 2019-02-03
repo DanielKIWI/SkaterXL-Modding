306b9357ee783b269daa95abb3d1ac3283c1cae2
Daniel Ivert <ivert.daniel@gmail.com>
2019-02-03 21:27:39 +0100

    Added DebugHelper

---
+++ XLShredMods/DebugGUI/Helper/DebugExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DebugGUI.Helper {
+    public static class DebugHelper {
+        private static string indentString = " | ";
+        public static void LogObjectHierarchy(GameObject go) {
+            Debug.Log(GetObjectHierarchyDescription(go, 0));
+        }
+        public static string GetObjectHierarchyDescription(GameObject gameObject, int indentLevel) {
+            string result = "";
+            for (int i = 0; i < indentLevel; i++) {
+                result += indentString;
+            }
+            result += gameObject.name + " (Components: [";
+            List<string> componentDescriptions = new List<string>();
+            foreach (var c in gameObject.GetComponents<Component>()) {
+                if (c is Transform) continue;
+                componentDescriptions.Add(c.GetType().Name);
+            }
+            result += string.Join(", ", componentDescriptions.ToArray());
+            result += "]";
+            if (gameObject.transform.childCount > 0) {
+                List<string> childDescriptions = new List<string>();
+                for (int i = 0; i < gameObject.transform.childCount; i++) {
+                    childDescriptions.Add(GetObjectHierarchyDescription(gameObject.transform.GetChild(i).gameObject, indentLevel + 1));
+                }
+                result += "\n" + string.Join("\n", childDescriptions.ToArray());
+            }
+            return result;
+        }
+    }
+}
--- XLShredMods/XLMultiplayerMod/SkaterCloner.cs
+++ XLShredMods/XLMultiplayerMod/SkaterCloner.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
+using System.Collections;
+using DebugGUI.Helper;
 
 namespace XLMultiplayerMod {
     public class SkaterCloner: MonoBehaviour {
@@ -12,15 +14,60 @@ namespace XLMultiplayerMod {
                 CopyPlayer();
             }
         }
+        //public bool DoesTransformGetRecorded(Transform t) {
+        //    if (t == null) return false;
+        //    foreach (var recordedTransform in ReplayManager.Instance.recorder.transformsToBeRecorded) {
+        //        if (recordedTransform == null) continue;
+        //        if (t.ToString() == recordedTransform.ToString()) return true;
+        //    }
+        //    return false;
+        //}
+        //public void DestroyNotRecordedTransforms(Transform t) {
+        //    if (!DoesTransformGetRecorded(t)) {
+        //        Destroy(t.gameObject);
+        //        return;
+        //    }
+        //    for (int i = 0; i < t.childCount; i++) {
+        //        DestroyNotRecordedTransforms(t.GetChild(i));
+        //    }
+        //}
         public void CopyPlayer() {
             try {
-                GameObject skaterRoot = PlayerController.Instance.transform.root.gameObject;// skaterController.skaterTransform.gameObject;
+                GameObject skaterRoot = PlayerController.Instance.transform.root.Find("Skater Root").gameObject;// skaterController.skaterTransform.gameObject;
+                List<Transform> list = new List<Transform>(PlayerController.Instance.respawn.getSpawn);
+                foreach (object obj in Enum.GetValues(typeof(HumanBodyBones))) {
+                    HumanBodyBones humanBodyBones = (HumanBodyBones)obj;
+                    if (humanBodyBones >= HumanBodyBones.Hips && humanBodyBones < HumanBodyBones.LastBone) {
+                        Transform boneTransform = PlayerController.Instance.animationController.skaterAnim.GetBoneTransform(humanBodyBones);
+                        if (!(boneTransform == null)) {
+                            list.Add(boneTransform);
+                        }
+                    }
+                }
+                foreach (Transform t in list) {
+                    t.gameObject.name = "IMPORTANT-" + t.gameObject.name;
+                }
+                DebugHelper.LogObjectHierarchy(skaterRoot);
+
+                return;
+                skaterRoot.AddComponent<ImportentTransformReferences>().importantTransforms = list.ToArray();
+                DebugHelper.LogObjectHierarchy(skaterRoot);
                 skaterRoot.SetActive(false);
                 GameObject skaterClone = GameObject.Instantiate(skaterRoot);
+                Destroy(skaterRoot.GetComponent<ImportentTransformReferences>());
+                Transform[] impotatTransforms = skaterClone.GetComponent<ImportentTransformReferences>().importantTransforms;
                 skaterClone.transform.SetParent(null);
                 skaterRoot.SetActive(true);
-                //skaterClone.SetActive(false);
-                Debug.Log("skaterClone: " + skaterClone);
+
+                foreach (Transform t in skaterClone.GetComponentsInChildren<Transform>()) {
+                    if (t == skaterClone) continue;
+                    if (Array.IndexOf(impotatTransforms, t) == -1) {
+                        Destroy(t.gameObject);
+                    }
+                }
+                //Destroy(skaterClone.transform.Find("Camera Rig").gameObject);
+                //DestroyNotRecordedTransforms(skaterClone.transform);
+
 
                 foreach (var c in skaterClone.GetComponentsInChildren<GraphicRaycaster>()) {
                     Debug.Log("Destroyed Component " + c + " from skaterClone");
@@ -54,6 +101,7 @@ namespace XLMultiplayerMod {
                     Destroy(c);
                 }
                 skaterClone.SetActive(true);
+                DebugHelper.LogObjectHierarchy(skaterClone);
             } catch (Exception e) {
                 Debug.LogException(e);
             }
