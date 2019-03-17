bd09e5ecd661704abce8939db07235c62dc02326
Daniel Ivert <ivert.daniel@gmail.com>
2019-03-17 16:42:56 +0100

    DebugGUI: - Added Inspector Window, - Added LineDrawer for Gizmos - Added [Rec] mark in hierarchy for transforms that are recorded by replayEditor (I probably will change this to a more generic way at some point)

--- XLShredMods/DebugGUI/DebugConsoleGUI.cs
+++ XLShredMods/DebugGUI/DebugConsoleGUI.cs
@@ -67,36 +67,34 @@ namespace DebugGUI {
                     width = boxWidth,
                     yMax = Screen.height - 20f
                 };
-                Rect viewRect = new Rect(10f, 10f, messageWidth, height);
-                GUI.Box(boxRect, "Console", this.boxStyle);
-
-                scrollPosition = GUI.BeginScrollView(boxRect, scrollPosition, viewRect, false, true);
-                float yOffset = 20f;
-                foreach (DebugConsoleGUI.StaticMessage message2 in this.messages) {
-                    this.font.normal.textColor = message2.color;
-                    float messageHeight = message2.CalcHeight(font, messageWidth);
-                    GUI.Label(new Rect(20f, yOffset, messageWidth, messageHeight), message2.getGuiContent(), this.font);
-                    yOffset += messageHeight;
+                //GUI.Box(boxRect, "Console", this.boxStyle);
+                GUILayout.BeginArea(boxRect, "Console", boxStyle);
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+                lock (messages) {
+                    foreach (var msg in messages) {
+                        this.font.normal.textColor = msg.color;
+                        float messageHeight = msg.CalcHeight(font, messageWidth);
+                        GUILayout.Label(msg.getGuiContent(), this.font);
+                    }
                 }
-                GUI.EndScrollView(true);
-                //float 
-                //    GUI.VerticalSlider()
+                GUILayout.EndScrollView();
+                GUILayout.EndArea();
             }
+
+            GUILayout.BeginArea(new Rect(20f, boxMinY - 20f, Screen.width - 40f, 20f));
             LogType[] logTypes = Enum.GetValues(typeof(LogType)).Cast<LogType>().ToArray();
-            float x = 20f;
-            float w = 0f;
             foreach (var type in logTypes) {
-                GUIContent c1 = new GUIContent(Enum.GetName(typeof(LogType), type));
-                w = GUI.skin.toggle.CalcSize(c1).x;
-                showLogType[type] = GUI.Toggle(new Rect(x, boxMinY - 20f, w, 20f), showLogType[type], c1);
-                x += w;
+                showLogType[type] = GUILayout.Toggle(showLogType[type], Enum.GetName(typeof(LogType), type));
             }
-            GUIContent c2 = new GUIContent("autoScrollToBottom");
-            w = GUI.skin.toggle.CalcSize(c2).x;
-            autoScroll = GUI.Toggle(new Rect(x, boxMinY - 20f, w, 20f), autoScroll, c2);
-            x += w;
+            autoScroll = GUILayout.Toggle(autoScroll, "autoScrollToBottom");
+            if (GUILayout.Button("Clear")) {
+                lock (messages) {
+                    this.messages.Clear();
+                }
+            }
+            GUILayout.EndArea();
         }
-        
+
         public static DebugConsoleGUI Instance {
             get {
                 return DebugConsoleGUI._instance;
@@ -106,45 +104,27 @@ namespace DebugGUI {
             if (!Main.enabled) return;
             UnityEngine.Debug.unityLogger.logEnabled = true;
             //Remove messages older than lifetime
-            foreach (StaticMessage msg in messages) {
-                if (Time.unscaledTime - this.messages[0].time > Main.settings.MessageLifeTime) {
-                    this.messages.RemoveAt(0);
-                }
-            }
+            messages = messages.Where(m => m.endTime > Time.unscaledTime).ToList();
             if (Input.GetKeyDown(KeyCode.F9)) {
                 Main.guiVisible = !Main.guiVisible;
             }
             if (Input.GetKeyDown(KeyCode.Delete)) {
-                if (this.messages == null) {
-                    this.messages = new List<DebugConsoleGUI.Message>();
-                } else {
+                lock (messages) {
                     this.messages.Clear();
                 }
             }
         }
-        
-        private void AddMessage(string message, Color color) {
-            this.messages.Add(new DebugConsoleGUI.StaticMessage(message, color));
 
-            if (this.messages.Count > Main.settings.MaxLogsCount) {
-                this.messages.RemoveAt(0);
-            }
-        }
-        private Color colorForLogType(LogType type) {
-            switch (type) {
-                case LogType.Exception: return Color.red;
-                case LogType.Error: return Color.red;
-                case LogType.Assert: return Color.black;
-                case LogType.Warning: return Color.yellow;
-                case LogType.Log: return Color.white;
-                default: return Color.white;
+        private void AddMessage(LogType logType, string message) {
+            lock (messages) {
+                this.messages.Add(new DebugConsoleGUI.StaticMessage(logType, message));
             }
         }
         public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) {
             parentHandler.LogFormat(logType, context, format, args);
             if (!Main.enabled) return;
             if (showLogType[logType]) {
-                AddMessage(Enum.GetName(typeof(LogType), logType) + ": " + String.Format(format, args), colorForLogType(logType));
+                AddMessage(logType, String.Format(format, args));
             }
         }
 
@@ -156,58 +136,77 @@ namespace DebugGUI {
             for (int i = 0; i < Mathf.Min(stackTrace.FrameCount, 4); i++) {
                 text = text + "\n" + stackTrace.GetFrame(i).ToString();
             }
-            AddMessage(text, colorForLogType(LogType.Exception));
+            AddMessage(LogType.Exception, text);
         }
 
 
         public abstract class Message {
+            public float endTime;
+            public Color color;
             public virtual float CalcHeight(GUIStyle style, float width) {
                 return style.CalcHeight(getGuiContent(), width);
             }
             public abstract GUIContent getGuiContent();
-            
-            public float time;
-            
-            public Color color;
         }
 
-        public class StaticMessage: Message {
-            public StaticMessage(string txt) {
-                this.text = txt;
-                this.time = Time.unscaledTime;
-                this.color = Color.white;
+        public class StaticMessage : Message {
+            private string text;
+            private Color colorForLogType(LogType logType) {
+                switch (logType) {
+                    case LogType.Exception:
+                    case LogType.Error: return Color.red;
+                    case LogType.Assert: return Color.black;
+                    case LogType.Warning: return Color.yellow;
+                    case LogType.Log: return Color.white;
+                    default: return Color.white;
+                }
             }
-            
+            private float lifeTimeFactorFor(LogType logType) {
+                switch (logType) {
+                    case LogType.Exception: return 10f;
+                    case LogType.Error: return 5f;
+                    case LogType.Assert: return 2f;
+                    case LogType.Warning: return 2f;
+                    case LogType.Log: return 1f;
+                    default: return 1f;
+                }
+            }
+
+            public StaticMessage(LogType logType, string txt) {
+                this.text = Enum.GetName(typeof(LogType), logType) + ": " + txt;
+                this.color = colorForLogType(logType);
+                this.endTime = Time.unscaledTime + Main.settings.MessageLifeTime * lifeTimeFactorFor(logType);
+            }
+
             public StaticMessage(string txt, Color color) {
                 this.text = txt;
-                this.time = Time.unscaledTime;
+                this.endTime = Time.unscaledTime;
                 this.color = color;
             }
             public override GUIContent getGuiContent() {
                 return new GUIContent(text);
             }
-            private string text;
         }
 
-        public class DynamicMessage : Message {
-            public DynamicMessage(Func<string> func) {
-                this.textFunc = func;
-                this.time = Time.unscaledTime;
-                this.color = Color.white;
-            }
-            
-            public DynamicMessage(Func<string> func, Color color) {
-                this.textFunc = func;
-                this.time = Time.unscaledTime;
-                this.color = color;
-            }
-            public override GUIContent getGuiContent() {
-                return new GUIContent(textFunc());
-            }
+        //public class DynamicMessage : Message {
+        //    public DynamicMessage(Func<string> func) {
+        //        this.textFunc = func;
+        //        this.EndTime = Time.unscaledTime;
+        //        this.color = Color.white;
+        //    }
 
-            // Token: 0x04001141 RID: 4417
-            private Func<string> textFunc;
-        }
+        //    public DynamicMessage(Func<string> func, Color color) {
+        //        this.textFunc = func;
+        //        this.EndTime = Time.unscaledTime;
+        //        this.color = color;
+        //    }
+        //    public override GUIContent getGuiContent() {
+        //        return new GUIContent(textFunc());
+        //    }
+
+        //    Token: 0x04001141 RID: 4417
+        //    private Func<string> textFunc;
+        //}
 
     }
     public static class MessageListExtension {
--- XLShredMods/XLShredManualBailRespawn/Main.cs
+++ XLShredMods/XLShredManualBailRespawn/Main.cs
@@ -16,6 +16,7 @@ namespace XLShredManualBailRespawn {
         public static bool enabled;
         public static String modId;
         public static bool visible;
+        private static HarmonyInstance harmonyInstance;
         // Send a response to the mod manager about the launch status, success or not.
         static void Load(UnityModManager.ModEntry modEntry) {
             modId = modEntry.Info.Id;
@@ -23,12 +24,17 @@ namespace XLShredManualBailRespawn {
 
             ModUIBox uiBoxKiwi = ModMenu.Instance.RegisterModMaker("com.kiwi", "Kiwi");
             uiBoxKiwi.AddToggle("Manual Bail Respawn (Xbox: A, PS4: X)", Side.left, () => enabled, false, (v) => visible = v);
-            
-            HarmonyInstance harmonyInstance = HarmonyInstance.Create(modId);
-            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+
+            harmonyInstance = HarmonyInstance.Create(modId);
         }
         static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
             enabled = value;
+            if (enabled) {
+                harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            } else {
+                if (harmonyInstance != null)
+                    harmonyInstance.UnpatchAll(harmonyInstance.Id);
+            }
             return true;
         }
     }
